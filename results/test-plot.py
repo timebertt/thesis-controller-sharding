25ca48a61dc007dd856efc75e26aefab39ef0224
Tim Ebert <timebertt@gmail.com>
2022-10-15 19:02:29 +0200

    More advanced plot demo

--- results/test-plot.py
+++ results/test-plot.py
@@ -3,27 +3,55 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# do subplots side by side
+plt.figure(figsize=(10,6))
+
+# subplot per project
+plt.subplot(121)
+
 # read data
 data = pd.read_csv(
-    "test-plot.csv",
+    "test-websites_per_project.csv",
     parse_dates=["ts"],
     date_parser=lambda col: pd.to_datetime(col, utc=True, unit="s"),
 )
+ts_min = data.ts.min()
 
-plt.figure(figsize=(10, 6))
+for project in data.namespace.unique():
+    d = data[data.namespace.eq(project)]
+    plt.plot((d.ts - ts_min).astype("timedelta64[s]"), d.value, label=project)
 
-for controller in data.controller.unique():
-    controller_data = data[data.controller.eq(controller)]
-    min_ts = controller_data.ts.min()
-
-    plt.plot((controller_data.ts - min_ts).astype("timedelta64[s]"), controller_data.value, label=controller)
-
-plt.title("Reconciliation Rate by Controller")
+plt.title("Websites per Project")
 plt.grid(True)
 plt.xlabel('Time in seconds')
-plt.ylabel('ops/s')
+plt.ylabel('count')
 plt.xlim([0, (data.ts.max() - data.ts.min()).seconds])
 plt.legend(loc="best")
+
+# subplot per shard
+plt.subplot(122)
+
+# read data
+data = pd.read_csv(
+    "test-websites_per_shard.csv",
+    parse_dates=["ts"],
+    date_parser=lambda col: pd.to_datetime(col, utc=True, unit="s"),
+)
+ts_min = data.ts.min()
+
+for shard in data.shard.unique():
+    d = data[data.shard.eq(shard)]
+    plt.plot((d.ts - ts_min).astype("timedelta64[s]"), d.value, label=shard)
+
+plt.title("Websites per Shard")
+plt.grid(True)
+plt.xlabel('Time in seconds')
+plt.ylabel('count')
+plt.xlim([0, (data.ts.max() - data.ts.min()).seconds])
+plt.legend(loc="best")
+
+# spacing
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.2, wspace=0.3)
 
 # export
 plt.savefig('test.pdf', bbox_inches="tight")
